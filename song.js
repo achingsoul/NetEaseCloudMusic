f9c1aeb8d53f8ad5864181b153a3ce0ffef2cb05
mengpeiyao <345004536@qq.com>
2017-08-31 08:19:39 +0800

    添加播放/暂停键

--- song.js
+++ song.js
@@ -22,6 +22,14 @@ $(function(){
     audio.src = 'http://osb6hhg60.bkt.clouddn.com/%E6%96%AF%E5%BE%B7%E5%93%A5%E5%B0%94%E6%91%A9%E6%83%85%E4%BA%BA-%E9%99%88%E5%A5%95%E8%BF%85.m4a'
     audio.oncanplay = function(){
         audio.play()
+        $('.disc-container').addClass('playing') // 音乐播放时转动碟片
     }
-    $('.disc-container').addClass('playing') // 音乐播放时转动碟片
+    $('.icon-pause').on('click', function(){
+        audio.pause()
+        $('.disc-container').removeClass('playing') 
+    })
+    $('.icon-play').on('click', function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    })
 })
