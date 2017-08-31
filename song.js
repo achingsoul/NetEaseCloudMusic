c877ba99a199a631f49c2706203b35561c62b6cb
mengpeiyao <345004536@qq.com>
2017-08-31 17:01:33 +0800

    更改歌曲链接

--- song.js
+++ song.js
@@ -19,7 +19,7 @@ $(function(){
     })
 
     let audio = document.createElement('audio')
-    audio.src = '//osb6hhg60.bkt.clouddn.com/%E6%96%AF%E5%BE%B7%E5%93%A5%E5%B0%94%E6%91%A9%E6%83%85%E4%BA%BA-%E9%99%88%E5%A5%95%E8%BF%85.m4a'
+    audio.src = '//dl.stream.qqmusic.qq.com/C400003TgAeV2YzOCj.m4a?vkey=D369B9844412360E5CC37CAA570BD18D428B6C5F234A3E7EB4E406847092FCEB8E0A13AAAA7EF3C4EA8111AFB6D3EC6B9A2C7A2C3B1A72DF&guid=5946636130&uin=345004536&fromtag=66'
     audio.oncanplay = function(){
         audio.play()
         $('.disc-container').addClass('playing') // 音乐播放时转动碟片
