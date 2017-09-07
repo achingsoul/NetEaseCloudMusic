dd759bb50dafe8c908d11bd5b9426e7f65710f60
mengpeiyao <345004536@qq.com>
2017-09-07 23:00:49 +0800

    tab切换

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-$(function(){
-    $.get('./songs.json').then(function(response){
+$(function () {
+    $.get('./songs.json').then(function (response) {
         let items = response
-        items.forEach((i)=>{
+        items.forEach((i) => {
             let $li = $(`
             <li>
                 <a href="./song.html?id=${i.id}">
@@ -20,7 +20,33 @@ $(function(){
             $('#latestMusic').append($li)
         })
         $('#latestMusicLoading').remove()
-    },function(){
+    }, function () {
 
+    })
+
+    $('.siteNav').on('click', 'ol.tabItems>li', function (e) {
+        let $li = $(e.currentTarget).addClass('active')
+        $li.siblings().removeClass('active')
+        let index = $li.index()
+        $li.trigger('tabChange', index)
+        $('.tabContent > li').eq(index).addClass('active').siblings().removeClass('active')
+    })
+
+    $('.siteNav').on('tabChange', function (e, index) {
+        let $li = $('.tabContent > li').eq(index)
+        if ($li.attr('data-downloaded') === 'yes') {
+            return
+        }
+        if (index === 1) {
+            $.get('./page2.json').then((response) => {
+                $li.text(response.content)
+                $li.attr('data-downloaded', 'yes')
+            })
+        } else if (index === 2) {
+            $.get('./page3.json').then((response) => {
+                $li.text(response.content)
+                $li.attr('data-downloaded', 'yes')
+            })
+        }
     })
 })
